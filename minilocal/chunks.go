99b9116ace80f103ac6fceecbec2e7fb6bdd5602
Robert Jacob <xperimental@solidproject.de>
2017-10-02 23:31:43 +0200

    Add reader for series chunks.

--- minilocal/chunks.go
+++ minilocal/chunks.go
@@ -10,15 +10,47 @@ import (
 	"github.com/prometheus/prometheus/storage/local/chunk"
 )
 
-// ReadChunks reads all chunks for a fingerprint.
-func ReadChunks(baseDir string, fpr model.Fingerprint) ([]chunk.Chunk, error) {
+// ChunkReadCloser provides an interface to read chunks from a series.
+type ChunkReadCloser interface {
+	Read() (chunk.Chunk, error)
+	Close() error
+}
+
+type chunkReader struct {
+	file    *os.File
+	chunks  int
+	current int
+}
+
+func (r *chunkReader) Read() (chunk.Chunk, error) {
+	if r.current == r.chunks {
+		return nil, io.EOF
+	}
+
+	r.current++
+	chunk, err := readChunk(r.file)
+	if err != nil {
+		return nil, err
+	}
+
+	return chunk, nil
+}
+
+func (r *chunkReader) Close() error {
+	r.chunks = 0
+	r.current = 0
+
+	return r.file.Close()
+}
+
+// NewReader creates a new reader to read chunks from a series.
+func NewReader(baseDir string, fpr model.Fingerprint) (ChunkReadCloser, error) {
 	fileName := fileNameForFingerprint(baseDir, fpr)
 
 	file, err := os.Open(fileName)
 	if err != nil {
 		return nil, err
 	}
-	defer file.Close()
 
 	fileInfo, err := file.Stat()
 	if err != nil {
@@ -29,23 +61,50 @@ func ReadChunks(baseDir string, fpr model.Fingerprint) ([]chunk.Chunk, error) {
 	chunkCount := int(fileSize / chunkLenWithHeader)
 	log.Printf("file size: %d chunks: %d", fileSize, chunkCount)
 
-	chunks := make([]chunk.Chunk, 0, chunkCount)
-	for i := 0; i < chunkCount; i++ {
-		buf := make([]byte, chunkLenWithHeader)
-		if _, err := io.ReadFull(file, buf); err != nil {
-			return nil, err
-		}
+	return &chunkReader{
+		file:    file,
+		chunks:  chunkCount,
+		current: -1,
+	}, nil
+}
 
-		chunk, err := chunk.NewForEncoding(chunk.Encoding(buf[chunkHeaderTypeOffset]))
-		if err != nil {
-			return nil, err
-		}
-		if err := chunk.UnmarshalFromBuf(buf[chunkHeaderLen:]); err != nil {
-			return nil, err
-		}
-		chunks = append(chunks, chunk)
+// ReadChunks reads all chunks for a fingerprint.
+func ReadChunks(baseDir string, fpr model.Fingerprint) ([]chunk.Chunk, error) {
+	reader, err := NewReader(baseDir, fpr)
+	if err != nil {
+		return nil, err
 	}
-	return chunks, nil
+	defer reader.Close()
+
+	chunks := []chunk.Chunk{}
+	for {
+		chunk, err := reader.Read()
+		switch {
+		case err == io.EOF:
+			return chunks, nil
+		case err != nil:
+			return nil, err
+		default:
+			chunks = append(chunks, chunk)
+		}
+	}
+}
+
+func readChunk(reader io.Reader) (chunk.Chunk, error) {
+	buf := make([]byte, chunkLenWithHeader)
+	if _, err := io.ReadFull(reader, buf); err != nil {
+		return nil, err
+	}
+
+	chunk, err := chunk.NewForEncoding(chunk.Encoding(buf[chunkHeaderTypeOffset]))
+	if err != nil {
+		return nil, err
+	}
+	if err := chunk.UnmarshalFromBuf(buf[chunkHeaderLen:]); err != nil {
+		return nil, err
+	}
+
+	return chunk, nil
 }
 
 func fileNameForFingerprint(basePath string, fp model.Fingerprint) string {
